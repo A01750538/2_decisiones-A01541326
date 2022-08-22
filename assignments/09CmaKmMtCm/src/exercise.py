e7794e2f77003bf843d20e4293b969203d718259
A01750538 <110841547+A01750538@users.noreply.github.com>
2022-08-22 14:11:16 -0500

    entrega

--- assignments/09CmaKmMtCm/src/exercise.py
+++ assignments/09CmaKmMtCm/src/exercise.py
@@ -3,19 +3,21 @@ def main():
     numero = int(input("Introduce los cm:"))
     
     if numero < 100:
-        print (numero , " cm")
+        print (f"{numero} cm")
     elif numero<100000: 
-        print (numero//100, " m")
+        print (f"{numero//100} m")
+    elif numero1 == numero//100000:
+        print (f"{numero1} km")
     else: 
         numero1 = numero//100000
-        print (numero1, " km")
+        print (f"{numero1} km")
         numero2= numero%100000
         if numero2> 0:
             numero3 = numero2//100
-            print (numero3," m")
+            print (f"{numero3} m")
             numero4 = numero2%100
             if numero > 0:
-                print (numero4*100, " cm")
+                print (f"{numero4*100} cm") 
 
 if __name__ == '__main__':
     main()
