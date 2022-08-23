c6810a0da2b4c085f7a47239803c09f820c9fd07
A01750538 <110841547+A01750538@users.noreply.github.com>
2022-08-22 21:01:54 -0500

    entrega 05

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -5,17 +5,29 @@ def main():
     z = int(input("Ingresa el tercer número:"))
 
     if(x<y and y<z):
-        print("",x,"-",y,"-",z)
+        print (x)
+        print (y)
+        print (z)
     elif (y<x and x<z):
-        print("",y,"-",x,"-",z)
+        print (y)
+        print (x)
+        print (z)
     elif(z<x and x<y):
-        print("",z,"-",x,"-",y)
+        print (z)
+        print (x)
+        print (y)
     elif(z<y and y<x):
-        print("",z,"-",y,"-",x)
+        print (z)
+        print (y)
+        print (x)
     elif(x<z and z<y):
-        print("",x,"-",z,"-",y)
+        print (x)
+        print (z)
+        print (y)
     elif(y<z and z<x):
-        print("",y,"-",z,"-",x)
+        print (y)
+        print (z)
+        print (x)
 
 if __name__=='__main__':
     main()
